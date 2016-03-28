b2680c09e015d9b23e66156a7e317c5aa454f235
James Stokes <213503636@stu.ukzn.ac.za>
2016-03-28 21:26:27 +0200

    new private repo sync

---
+++ Time.cpp
@@ -0,0 +1,46 @@
+#include "Time.h"
+
+using namespace std;
+
+Time::Time()
+{
+}
+
+Time::Time(int hours, int minutes) {
+	this->hours = hours;
+	this->minutes = minutes;
+}
+
+void Time::get(int* pH, int* pM) {
+	*pH = hours;
+	*pM = minutes;
+}
+
+Time Time::set(int hours, int minutes) {
+	this->hours = hours;
+	this->minutes = minutes;
+}
+
+ostream& operator<<(ostream& os, Time & time)
+{
+	os << time.hours << ":" << time.minutes;
+	return os;
+}
+
+Time operator++(Time & oldTime)
+{
+	Time time = oldTime;
+	int* pH;
+	int* pM;
+	int hours;
+	int mins;
+	pH = &hours;
+	pM = &mins;
+	time.get(pH, pM);
+	time.set(hours, mins++);
+	return time;
+}
+
+Time::~Time()
+{
+}
---
+++ Time.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+class Time
+{
+private:
+	int hours;
+	int minutes;
+
+public:
+	Time();
+	Time(int, int);
+	void get(int* ,int*);
+	Time set(int, int);
+	friend ostream& operator<<(ostream&, Time&);
+	friend Time operator++(Time&);
+	~Time();
+};
+
